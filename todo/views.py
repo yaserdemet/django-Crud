342487ba8e7a9809cfaa314fdcacd86c99bf1539
yaser <ddemety372@gmail.com>
2022-08-31 01:53:11 +0300

    update

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from turtle import update
 from django.shortcuts import render , redirect
 
 # Create your views here.
@@ -28,3 +29,37 @@ def todo_create(request):
         "form" : form
     }
     return render (request , "todo/todo_add.html" , context)
+
+# id delete ve update gereklidir
+def todo_update(request , id):
+    todo = Todo.objects.get(id=id)
+    # !burada  update edilecek elemanı objetc.get(id=id) ile aldık
+    form = TodoForm(instance=todo)
+    # !burada ise bilgileri bir forma koyup göstermem gerek. bu yüzden TodoForm un içine attık
+    
+    if request.method == "POST":
+        form = TodoForm(request.POST , instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    
+    
+    context = {
+        "todo" : todo,
+        "form"  : form
+    }
+    
+    return render(request , "todo/todo_update.html" , context)
+
+
+def todo_delete(request , id):
+    todo = Todo.objects.get(id = id)
+    if request.method == "POST":
+        todo.delete()
+        return redirect("home")
+    
+    context = {
+        "todo" : todo
+    }
+    return render(request , "todo/todo_delete.html" , context)
+    
