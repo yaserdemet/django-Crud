52872197f9f7239bc240e7234613bb1f90c89b62
yaser <ddemety372@gmail.com>
2022-08-31 00:30:25 +0300

    first commit

---
+++ todo/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render , redirect
+
+# Create your views here.
+
+from django.http import HttpResponse
+from .models import Todo
+from .forms import TodoForm
+
+def home(request):
+    todos = Todo.objects.all()
+    form = TodoForm()
+    context = {
+        "todos" : todos,
+        "form" : form
+    }
+    return render (request , "todo/home.html" , context)
+
+
+def todo_create(request):
+    form = TodoForm()
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    
+    context = {
+        "form" : form
+    }
+    return render (request , "todo/todo_add.html" , context)
